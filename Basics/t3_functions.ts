50273769c29392914bbc4d8706b4f7798f54cccb
jayant413 <jayantsawarkar4103@gmail.com>
2023-07-06 10:34:16 +0530

    functions

--- Basics/t3_functions.ts
+++ Basics/t3_functions.ts
@@ -1,4 +1,4 @@
-function addTwo(num : number){
+function addTwo(num : number) : number{
     return num + 2
 }
 
@@ -12,5 +12,36 @@ function signUpUser (name:string , email:string , age : number , isPaid : boolea
 addTwo(5)
 signUpUser("Jayant" , "jay4103@gmail.com" , 20 , true)
 console.log(getUpper("Jayant"))
+
+
+// function getValue (myVal : number) : boolean{
+//     if(myVal > 5){
+//         return true
+//     }
+//     return "200 OK"
+// }
+
+
+const getHello = (s : string  ) : string =>{
+    return "200 OK"
+}
+
+
+const heros = ["thor" , "spiderman" , "ironman"]
+
+heros.map((hero : string) : string=>{
+    return `hero is ${hero}`
+})
+
+
+function consoleError(errmsg : string) : void{
+    console.log(errmsg);
+}
+function handleError(errmsg : string) : never{
+    throw new Error(errmsg)
+}
+
+
+
 
 export {}
